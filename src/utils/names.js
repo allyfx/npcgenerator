cb70ead7bb0699a1df1cb8e5e6d25afeb86fd922
Alícia Foureaux <aliciafoureauxlucas@gmail.com>
2023-06-04 08:19:15 -0300

    chore: create databases

---
+++ src/utils/names.js
@@ -0,0 +1,20 @@
+const M_NAMES = [
+  'Dane', 'Josef', 'Sebastian', 'Malcom', 'Colin', 'Victor', 'Atreus'
+]
+
+const F_NAMES = [
+  'Jane', 'Shelly', 'Daniela', 'Muriel', 'Justina', 'Salome', 'Renata'
+]
+
+const NAMES = {
+  M: M_NAMES,
+  F: F_NAMES
+}
+
+const SURNAMES = [
+  'Blanchard', 'Riva', 'Alting', 'Rose', 'Marchioni', 'Atwater', 'Dickens',
+  'Goebel', 'Hermansson', 'Dragomirov', 'Rowbottom', 'Olmos', 'Herrero', 'Antonio',
+  'Donne', 'Leong', 'Maksimov'
+]
+
+export { NAMES, SURNAMES }
